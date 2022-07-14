50aeaa2a03804a69a20dbba9e3f0dc0add320967
Eugene Zhang <zhang.eug@gmail.com>
2022-07-13 23:49:18 -0400

    Use two pointers approach instead

--- leetcode/arrays/sort_colours.py
+++ leetcode/arrays/sort_colours.py
@@ -3,22 +3,27 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # using bubble sort --> side by side comparison, swap if larger
         
-        # optimization --> sort can end early if swapped stays False
-        swapped = True
+        # using two pointers approach
         
-        # optimization --> after each iteration decrease last unsorted num by one
-        last_unsorted_num = len(nums) - 1
+        # start pointer and end pointer
+        start = 0
+        end = len(nums) - 1
         
-        while swapped:
+        increment = 0
         
-            swapped = False
-            
-            for i in range(last_unsorted_num):
-                if nums[i] > nums[i+1]:
-                    nums[i], nums[i+1] = nums[i+1], nums[i]
-                    
-                    swapped = True
-            
-            last_unsorted_num -= 1
+        while increment <= end:
+            if nums[increment] == 0:
+                nums[increment], nums[start] = nums[start], nums[increment]
+                
+                start += 1
+                
+            if nums[increment] == 2:
+                nums[increment], nums[end] = nums[end], nums[increment]
+
+                end -= 1
+                
+                # instead of incrementing here, go again
+                continue
+                
+            increment += 1
